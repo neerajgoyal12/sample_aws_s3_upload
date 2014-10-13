77dd0cb85e7a68a1eabc7e6a12fdb00462ee7f62
Neeraj Goyal12 <neerajgoyal12@gmail.com>
2014-10-13 18:17:38 +0530

    - minor comment

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,6 @@
+# config/initializers/aws.rb
 require 'aws-sdk'
 Rails.configuration.aws = YAML.load(ERB.new(File.read("#{Rails.root}/config/aws.yml")).result)[Rails.env].symbolize_keys!
 AWS.config(logger: Rails.logger)
 AWS.config(Rails.configuration.aws)
+
